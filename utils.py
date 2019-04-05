b8a5ac31bd601f2d15fb439f40007e878f399a91
marcocaserta <marcocaserta@gmail.com>
2019-04-05 06:40:32 +0000

    Added fastText implementation.

--- utils.py
+++ utils.py
@@ -8,22 +8,31 @@ from os import path, listdir
 class Config(object):
     # intermediate processing folders
     arxiv_dir =os.path.join('data', 'pdf', 'arxiv')
+    #  arxiv_dir =os.path.join('data', 'pdf', 'arxiv2')
     db_dir   = os.path.join('data','db.p')
     pArxiv_dir   = os.path.join('data','pArxiv')
+    #  pArxiv_dir   = os.path.join('data','pArxiv2')
     pdf_dir = os.path.join('data', 'pdf')
     txt_dir = os.path.join('data', 'txt')
     models_dir   = os.path.join('models')
     p_dir   = os.path.join('data','p')
+    pAbstract_dir   = os.path.join('data','pAbstract')
     #prefix = path.expanduser("~/gdrive/research/nlp/data/")
-    prefix = path.expanduser("/home/mcaserta/research/nlp/matheur")
+    prefix = path.expanduser("/home/mcaserta/research/matheur")
     prefixDict = path.expanduser("/home/mcaserta/research/nlp/data")
     vocab_folder = "google_vocab/"
     mappingFile = "mapping.csv"
     mappingArxiv = "mappingArxiv.csv"
+    distanceMatrixFile = "distanceMatrix.csv"
+    sortedDIstanceFile = "sortedDistance.csv"
     ngramsFile = "myngrams.txt"
     acronsFile = "myacrons.txt"
     specialFile = "myspecialwords.txt"
     nameModel  = "doc2vec.model"
 
-    biblioTitle = ["Bibliography", "References", "Acknowledgments", "Acknowledgements"]
+    biblioTitle = ["Bibliography", "References", "Acknowledgments",
+    "Acknowledgements", 'BIBLIOGRAPHY', 'REFERENCES', 'ACKNOWLEDGEMENTS',
+    'ACKNOWLEDGMENTS', 'Reference', 'REFERENCE']
+
+    abstractTitle = ["Abstract", "ABSTRACT", "Summary", "SUMMARY", "a b s t r a c t", "A B S T R A C T"]
 
